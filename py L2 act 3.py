2169384caf849ba3f14a999ef23fc46c07c4c4d1
yushra0 <zayaanyushra@gmail.com>
2025-06-19 23:47:53 +0600

    Add files via upload

---
+++ py L2 act 3.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name + last_name
+example = "Haa" * 5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String Multiplied 5 times gives this result :", example)
+
+word = "Coding"
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0:3])
